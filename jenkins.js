7c98247f800ae1dcdaf5950972b3412f19560b31
mcbrown87 <michael.c.brown87@gmail.com>
2017-06-05 19:31:17 -0400

    initial commit

---
+++ jenkins.js
@@ -0,0 +1,60 @@
+class JenkinsEndpoint {
+    constructor(url) {
+        this._url = url + "/api/json"
+    }
+
+    _getData() {
+        return new Promise((resolve, reject) => {
+            loadJSON(this._url, (data) => {
+                resolve(data)
+            })
+        })
+    }
+}
+
+class JenkinsJob extends JenkinsEndpoint {
+    constructor(url) {
+        super(url)
+    }
+
+    get score() {
+        return new Promise((resolve, reject) => {
+            this._getData().then((jenkinsJob) => {
+                resolve(jenkinsJob.healthReport[0].score)
+            })
+        })
+    }
+
+    get latestCompletedBuild() {
+        return new Promise((resolve, reject) => {
+            this._getData().then((jenkinsJob) => {
+                resolve(new JenkinsJobInstance(jenkinsJob.lastCompletedBuild.url))
+            })
+        })
+    }
+}
+
+class JenkinsJobInstance extends JenkinsEndpoint {
+    constructor(url) {
+        super(url)
+        this._testReportEndpoint = url + '/testReport'
+    }
+
+    get testReport() {
+        return new JenkinsTestReport(this._testReportEndpoint)
+    }
+}
+
+class JenkinsTestReport extends JenkinsEndpoint {
+    constructor(url) {
+        super(url)
+    }
+
+    get totalTestCount() {
+        return new Promise((resolve, reject) => {
+            this._getData().then((testReport) => {
+                resolve(testReport.passCount + testReport.failCount)
+            })
+        })
+    }
+}
